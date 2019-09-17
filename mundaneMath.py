bdcfba542b58e58c511fc026613f12ecdc239746
manoble7 <54915974+manoble7@users.noreply.github.com>
2019-09-17 06:08:11 -0400

    Add files via upload

---
+++ mundaneMath.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 17 05:59:11 2019
+
+@author: madelinenoble
+"""
+total = 0
+for i in range(0,100,2):
+    total = total + i
+print(total)
